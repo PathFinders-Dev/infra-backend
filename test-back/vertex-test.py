40c07ddf4d005d252f7743e65cd3da86a7f14438
Clear-Wisdom <seoho38hwang@gmail.com>
2025-04-26 19:22:57 +0900

    VertexAI sample code update

---
+++ test-back/vertex-test.py
@@ -0,0 +1,53 @@
+from google import genai
+from google.genai import types
+import base64
+
+def generate():
+  client = genai.Client(
+      vertexai=True,
+      project="solutionchallenge-pioneers",
+      location="global",
+  )
+
+  msg1_text1 = types.Part.from_text(text="""hi, how are you?""")
+  si_text1 = """you're gonna be the answer for the test model. when you got any messages, please return any random one sentence."""
+
+  model = "gemini-2.0-flash-001"
+  contents = [
+    types.Content(
+      role="user",
+      parts=[
+        msg1_text1
+      ]
+    ),
+  ]
+  generate_content_config = types.GenerateContentConfig(
+    temperature = 1,
+    top_p = 0.95,
+    max_output_tokens = 100,
+    response_modalities = ["TEXT"],
+    safety_settings = [types.SafetySetting(
+      category="HARM_CATEGORY_HATE_SPEECH",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_DANGEROUS_CONTENT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+      threshold="OFF"
+    ),types.SafetySetting(
+      category="HARM_CATEGORY_HARASSMENT",
+      threshold="OFF"
+    )],
+    system_instruction=[types.Part.from_text(text=si_text1)],
+  )
+
+  for chunk in client.models.generate_content_stream(
+    model = model,
+    contents = contents,
+    config = generate_content_config,
+    ):
+    print(chunk.text, end="")
+
+answer = generate()
+print(answer)
